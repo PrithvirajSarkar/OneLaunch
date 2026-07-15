63aad7bfb522a523dfc3ab2b0ea18c1deb0bf72b
Prithviraj Sarkar <prithvirajsarkar49@gmail.com>
2026-07-15 20:28:34 +0530

    Implement Gson JSON storage and loading

--- src/onelaunch/service/StorageManager.java
+++ src/onelaunch/service/StorageManager.java
@@ -1,39 +1,22 @@
 package onelaunch.service;
 
 import java.util.ArrayList;
-
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
 import onelaunch.model.Application;
 import onelaunch.model.Workspace;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 public class StorageManager {
 
     private static final String FILE_PATH = "C:\\Users\\user\\OneDrive\\Desktop\\FILES\\OneLaunch\\data\\workspaces.json";
 
-    public void saveWorkspaces(ArrayList<Workspace> workspaces) {
-        String data = "";
-
-for (Workspace work : workspaces) {
-
-    String workspaceName = work.getName();
-
-    ArrayList<Application> apps = work.getApplications();
-
-    data += "Workspace: " + workspaceName + "\n";
-    data += "Applications:\n";
-
-    for (Application app : apps) {
-
-        String applicationName = app.getName();
-        String applicationPath = app.getPath();
-
-        data += applicationName + "\n";
-        data += applicationPath + "\n\n";
-
-    }
-
-    data += "\n";
-}
+public void saveWorkspaces(ArrayList<Workspace> workspaces) {
+    Gson gson = new Gson();
+    
+    String data = gson.toJson(workspaces);
 // NOW ACTUALLY WRITE THE DATA TO THE FILE
     try {
         FileWriter writer = new FileWriter(FILE_PATH);
@@ -47,7 +30,34 @@ for (Workspace work : workspaces) {
     }
 }
 
-    public ArrayList<Workspace> loadWorkspaces() {
+public ArrayList<Workspace> loadWorkspaces() {
+
+    Gson gson = new Gson();
+
+    try {
+
+        FileReader reader = new FileReader(FILE_PATH);
+
+        Type workspaceListType =
+                new TypeToken<ArrayList<Workspace>>() {}.getType();
+
+        ArrayList<Workspace> workspaces =
+                gson.fromJson(reader, workspaceListType);
+
+        reader.close();
+
+        if (workspaces == null) {
+            return new ArrayList<>();
+        }
+
+        return workspaces;
+
+    } catch (IOException e) {
+
+        e.printStackTrace();
+
         return new ArrayList<>();
+
     }
+}
 }
